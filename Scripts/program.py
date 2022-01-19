9fad832df31d527f4c7e83455102b8d30006322b
Curvu <69159895+Curvu@users.noreply.github.com>
2022-01-19 10:15:25 +0000

    Update program.py

--- Scripts/program.py
+++ Scripts/program.py
@@ -1,4 +1,4 @@
-#! Made by Curvu
+#! @author Curvu
 #! The things that are commented out (that are code) I'll try to use them in the future
 import subprocess
 import serial
@@ -17,11 +17,12 @@ def stop_App(systray, item):
     systray.stop()
     exit()
 '''
-
+#$ Find the correct port
 def findPort():
-    global serialPort
-    i = 1
-    while i <= 6:
+    global serialPort ## global variable so i can use in the other functions
+    i = 1 ## Interval of ports: [1,6]
+    limit = 6 ## But you can change the the variable 'limit' and put 7, 8, etc.
+    while i <= limit:
         port = 'COM' + str(i) ## just switchig the port
         try:
             serialPort = serial.Serial(port, baudrate = 9600, timeout=0) ## connect to serial
@@ -30,6 +31,8 @@ def findPort():
         except OSError:
             i += 1
 
+#$ Read from serial port
+##
 def deck(red, white):
     line = serialPort.readline().strip() ## read line from serialPort
     if(line == b'R'): ## in arduino program if i click in button red it 'Serial.println('R')
@@ -44,11 +47,13 @@ def deck(red, white):
             print('Opening the white button program...')
         except OSError:
             print('Could not open program in the WHITE button')
-
+      
+#! You may change the value on 'red' and 'white' variables
 red = 'C:\\Users\\Utilizador\\AppData\\Local\\Discord\\Update.exe --processStart Discord.exe'
 white = ''
 
+#! Might have some problems but i'll try to fix them later
 findPort() ## call function to find the correct port
 while True: ## forever loop
-    deck(red, white)
+    deck(red, white) ## call function to start reading from port
 serialPort.close() ## disconnect from serial port
